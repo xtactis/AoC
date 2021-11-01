c24aa05e24f3d1cffa341a52c0f3860b10558b0b
Matija Dizdar <xtactis@gmail.com>
2021-11-02 00:36:48 +0900

    day 1, a couple of cosmetic changes nothing important, part1 is much nicer now tho

--- 2015/01.ml
+++ 2015/01.ml
@@ -4,24 +4,23 @@ let read_file filename =
     close_in ch;
     s
 
-let part1 = List.fold_left (fun acc x -> if x = '(' then acc+1 else acc-1) 0
+let part1 = List.fold_left (+) 0
 
 let part2 l =
-  let ftype c = if c = '(' then 1 else -1 in
-  let rec helper index floor = function
+  let rec find_neg index floor = function
     | [] -> -1
     | h::t ->
-       if floor+(ftype h) < 0 then index else
-         helper (index+1) (floor+(ftype h)) t in
-  helper 1 0 l
+       if floor+h < 0 then index else find_neg (index+1) (floor+h) t in
+  find_neg 1 0 l
 
-let explode s =
+let convert s =
+  let convert_one c = if c = '(' then 1 else -1 in
   let rec helper i l =
-    if i < 0 then l else helper (i-1) (s.[i] :: l) in
+    if i < 0 then l else helper (i-1) ((convert_one s.[i]) :: l) in
   helper (String.length s - 1) []
 
 let () =
-  let input = read_file "01.in" in
-  let exploded = explode input in
-  Printf.printf "Part 1: %d\nPart 2: %d\n" (part1 exploded) (part2 exploded)
+  let raw_input = read_file "01.in" in
+  let input = convert raw_input in
+  Printf.printf "Part 1: %d\nPart 2: %d\n" (part1 input) (part2 input)
     
