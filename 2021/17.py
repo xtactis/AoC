51a4210d2ec22bc45f0409697be3e5e5939d60b7
xtactis <xtactis@gmail.com>
2021-12-18 13:20:04 +0900

    yyeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaaaa

--- 2021/17.py
+++ 2021/17.py
@@ -9,23 +9,23 @@ def sim(xv, yv):
     for _ in range(300):
         px += xv
         py += yv
-        maxy = max(maxy, py)
         if xv > 0: xv -= 1
         yv -= 1
-        
-        # these two lines of code bring execution time from ~5 seconds to ~90ms
-        if xv == 0 and not (xs[0] <= px <= xs[1]): return False, None
-        if px > xs[1] or py < ys[0]: return False, None
 
-        if xs[0] <= px <= xs[1] and ys[0] <= py <= ys[1]: return True, maxy
-    return False, None
+        # these two lines of code bring execution time from ~5 seconds to ~90ms
+        if xv == 0 and not (xs[0] <= px <= xs[1]): return None
+        if px > xs[1] or py < ys[0]: return None
+
+        maxy = max(maxy, py)
+        if xs[0] <= px <= xs[1] and ys[0] <= py <= ys[1]: return maxy
+    return None
 
 p1 = 0
 p2 = 0
 for i in range(0, xs[1]+1):
     for j in range(-abs(ys[0]), abs(ys[0])):
-        good, val = sim(i, j)
-        if not good: continue
+        val = sim(i, j)
+        if val is None: continue
         p2 += 1
         p1 = max(p1, val)
 
