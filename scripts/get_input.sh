cd21ce7e6521ddca269510042fb18c1a0a29bce8
xtactis <matija.dizdar@protonmail.com>
2022-12-07 01:16:33 +0900

    added some scripts for automating annoying bits of aoc

---
+++ scripts/get_input.sh
@@ -0,0 +1,9 @@
+cookie=$AOC_COOKIE
+user_data="github.com/xtactis/AoC by matija.dizdar@pm.me"
+
+day=${1:-$(date +%d)}
+year=${2:-$(date +%Y)}
+
+echo "curl -A user_data -b $cookie https://adventofcode.com/$year/day/$day/input"
+curl -s -A user_data -b $cookie https://adventofcode.com/$year/day/$day/input -o $(printf "%02d.in" $day)
+
