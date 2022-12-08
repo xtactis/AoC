efe475cb84d5013e82c1bd64ee862c0553fa436f
xtactis <matija.dizdar@protonmail.com>
2022-12-09 00:07:59 +0900

    user data omg

--- scripts/answer.sh
+++ scripts/answer.sh
@@ -6,5 +6,5 @@ day=${2:-$(date +%-d)}
 year=${3:-$(date +%Y)}
 answer=`cat | tail -n 1`
 
-curl -s -b $cookie -A $user_data -X POST -d "level=$level&answer=$answer" https://adventofcode.com/$year/day/$day/answer | lynx -stdin --dump --nolist -width 200 | tail -n 1
+curl -s -b "$cookie" -A "$user_data" -X POST -d "level=$level&answer=$answer" https://adventofcode.com/$year/day/$day/answer | lynx -stdin --dump --nolist -width 200 | tail -n 1
 
--- scripts/get_input.sh
+++ scripts/get_input.sh
@@ -1,9 +1,10 @@
+set -x
+
 cookie=$AOC_COOKIE
 user_data="github.com/xtactis/AoC by matija.dizdar@pm.me"
 
 day=${1:-$(date +%-d)}
 year=${2:-$(date +%Y)}
 
-echo "curl -A user_data -b $cookie https://adventofcode.com/$year/day/$day/input"
-curl -s -A user_data -b $cookie https://adventofcode.com/$year/day/$day/input -o $(printf "%02d.in" $day)
-
+#echo "curl -A $user_data -b $cookie -o $(printf "%02d.in" $day) https://adventofcode.com/$year/day/$day/input"
+curl -A "$user_data" -b $cookie -o $(printf "%02d.in" $day) https://adventofcode.com/$year/day/$day/input
