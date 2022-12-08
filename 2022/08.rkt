dfc0f75c4a86fa34deaf38cee095e11bfb26eedb
Matija Dizdar <matija.dizdar@protonmail.com>
2022-12-08 19:50:36 +0900

    pretty stupid solution but it is what it is

---
+++ 2022/08.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(require threading)
+
+(define (addf x) (list x #f))
+
+(define input (for/list ([lst (in-lines)])
+                        (~>> lst
+                             string->list
+                             (map char->integer)
+                             (map (curry + -48))
+                             (map addf))))
+
+(define (find_visible_trees line height)
+  (if (empty? line)
+    '()
+    (append (list (list (caar line) (or (cadar line) (> (caar line) height))))
+            (find_visible_trees (rest line) (max (caar line) height)))))
+
+(define (transpose mat)
+  (apply map list mat))
+
+(define (rotate mat) 
+  (reverse (transpose mat)))
+
+(define (find_all_visible_trees mat)
+  (for/list ([line mat])
+            (find_visible_trees line -1)))
+
+(count identity (flatten (~>> input
+                              find_all_visible_trees
+                              rotate
+                              find_all_visible_trees
+                              rotate
+                              find_all_visible_trees
+                              rotate
+                              find_all_visible_trees
+                              (map (curry map cdr)))))
+
+(define (count_visible_trees line height)
+  (define taketh (takef line (lambda (p) (< (car p) height))))
+  (min (length line) (+ 1 (length taketh))))
+
+(foldl max 0 (for*/list ([i (in-range  (length input))]
+                         [j (in-range  (length (first input)))])
+                        (define row (list-ref input i))
+                        (define col (list-ref (transpose input) j))
+                        (define cur (car (list-ref row j)))
+                        (* (count_visible_trees (reverse (take row j)) cur)
+                           (count_visible_trees (rest (drop row j)) cur)
+                           (count_visible_trees (reverse (take col i)) cur)
+                           (count_visible_trees (rest (drop col i)) cur))))
+
