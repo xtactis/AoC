59b1ee920116ca2064d17a43bbb96d121158a223
Matija Dizdar <matija.dizdar@protonmail.com>
2022-12-15 15:23:15 +0900

    ~12 seconds I guess
     Changes to be committed:
    	new file:   15.py

---
+++ 2022/15.py
@@ -0,0 +1,27 @@
+import sys
+
+inp = [[int(e.split("=")[1][:-1]) for i, e in enumerate(l.split(" ")) if i in [2, 3, 8, 9]] for l in sys.stdin.readlines()]
+
+def solve(y):
+    ranges = []
+    for x1, y1, x2, y2 in inp:
+        dist = abs(y2-y1)+abs(x2-x1)
+        w = dist-abs(y1-y)
+        if w < 0:
+            continue
+        ranges.append([x1-w, x1+w])
+    ranges.sort()
+    left, right = ranges[0]
+    for r in ranges[1:]:
+        if right >= r[0]:
+            right = max(r[1], right)
+        else:
+            return (right+1)*4000000+y, True
+    return right-left, False
+
+print(solve(2000000)[0])
+for y in range(0, 4000001):
+    ans, part2 = solve(y)
+    if part2:
+        print(ans)
+        break
