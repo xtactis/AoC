3a9a248c2e79c74a01c56d48b94105ebdc5431d5
Matija Dizdar <matija.dizdar@protonmail.com>
2024-12-01 10:23:06 +0100

    day 1 2024, but actually this time

--- 2024/day01.d
+++ 2024/day01.d
@@ -1,7 +1,18 @@
-import std.stdio, std.array, std.algorithm, std.conv;
+import std.stdio, std.array, std.algorithm, std.conv, std.range, std.math;
 
-void main()
-{
-    int[] line = readln.split.map!(s => parse!int(s)).array;
-    writeln(line[0]);
+void main() {
+    string line;
+    int[] a;
+    int[] b;
+    int[int] count;
+    while ((line = readln()) !is null) {
+        int[] ns = line.split.map!(a => parse!int(a)).array();
+        a ~= ns[0];
+        b ~= ns[1];
+        count[ns[1]] += 1;
+    }
+    int part1 = zip(a.sort(), b.sort()).map!(d => abs(d[0]-d[1])).sum();
+    writeln("part 1: ", part1);
+    int part2 = a.map!(el => el * count.get(el, 0)).sum();
+    writeln("part 2: ", part2);
 }
