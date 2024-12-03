081b14097d3f9dd90ebf62e1f35824b03f7cd9b3
Matija Dizdar <matija.dizdar@protonmail.com>
2024-12-03 09:00:37 +0100

    Marginally nicer solution to day03 2024

--- 2024/day03.d
+++ 2024/day03.d
@@ -4,50 +4,59 @@ auto readLines(string file) {
     return readText(file).split("\n").filter!(l => l.length != 0);
 }
 
+int solvePart1(string line) {
+    int ret = 0;
+    while (!line.empty) {
+        int a = 0, b = 0;
+        long firstMul = countUntil(line, "mul(");
+        if (firstMul == -1) break;
+        line = line[firstMul+4..$];
+        try {
+            a = parse!(int)(line);
+        } catch (ConvException) {
+            continue;
+        }
+        if (a > 999) continue;
+        if (line[0] != ',') continue;
+        line = line[1..$];
+        try {
+            b = parse!(int)(line);
+        } catch (ConvException) {
+            continue;
+        }
+        if (b > 999) continue;
+        if (line[0] != ')') continue;
+        line = line[1..$];
+
+        ret += a*b;
+    }
+    return ret;
+}
+
 void main() {
     int part1 = 0;
     int part2 = 0;
-    bool enabled = true;
-    foreach (line; readLines("input/03.in")) {
-        while (!line.empty) {
-            int a = 0, b = 0;
-            long firstDo = countUntil(line, "do()");
-            long firstDont = countUntil(line, "don't()");
-            long firstMul = countUntil(line, "mul(");
-            if (firstMul == -1) break;
-            if (enabled) {
-                if (firstDont != -1 && firstDont < firstMul) {
-                    line = line[firstDont+7..$];
-                    enabled = false;
-                    continue;
-                }
-            } else {
-                if (firstDo == -1) break;
-                line = line[firstDo+4..$];
-                enabled = true;
-                continue;
-            }
-            line = line[firstMul+4..$];
-            try {
-                a = parse!(int)(line);
-            } catch (ConvException) {
-                continue;
-            }
-            if (a > 999) continue;
-            if (line[0] != ',') continue;
-            line = line[1..$];
-            try {
-                b = parse!(int)(line);
-            } catch (ConvException) {
-                continue;
-            }
-            if (b > 999) continue;
-            if (line[0] != ')') continue;
-            line = line[1..$];
+    string line = readText("input/03.in");
+    part1 += solvePart1(line);
 
-            part1 += a*b;
-        }
+    auto getIntervalEnd = (ref string line) {
+        long firstDont = countUntil(line, "don't()");
+        return firstDont == -1 ? line.length : firstDont+7;
+    };
+    long end = getIntervalEnd(line);
+    part2 += solvePart1(line[0..end]);
+    writeln(line[0..end]);
+    line = line[end..$];
+    while (!line.empty) {
+        long firstDo = countUntil(line, "do()");
+        if (firstDo == -1) break;
+        line = line[firstDo..$];
+        end = getIntervalEnd(line);
+        writeln(line[0..end]);
+        part2 += solvePart1(line[0..end]);
+        line = line[end..$];
     }
+
     writeln("part 1: ", part1);
     writeln("part 2: ", part2);
 }
