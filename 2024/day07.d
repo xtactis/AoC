89d43713eee8035cee5de23cfcd50a3725911385
Matija Dizdar <matija.dizdar@protonmail.com>
2024-12-07 15:07:17 +0100

    day07 2024 30x speedup by removing dp and simplifying cat significantly

--- 2024/day07.d
+++ 2024/day07.d
@@ -5,42 +5,22 @@ auto readLines(string file) {
     return readText(file).split("\n").filter!(l => l.length != 0);
 }
 
-struct state {
-    long sum;
-    int i;
-};
-
-bool[state] dp;
-bool possible(long target, long[] l, long sum = 0, int i = 0) {
-    if (i == l.length) return sum == target;
-    state cur = state(sum, i);
-    if (cur in dp) return dp[cur];
-    return dp[cur] = possible(target, l, sum + l[i], i + 1)
-                  || possible(target, l, sum == 0 ? l[i] : sum * l[i], i + 1);
-}
-
 long cat(long a, long b) {
     if (a == 0) return b;
 
-    int[] d;
-    while (b) {
-        d ~= (b % 10);
-        b /= 10;
+    long d = 1;
+    while (d <= b) {
+        d *= 10;
     }
-    foreach_reverse (digit; d) {
-        a *= 10;
-        a += digit;
-    }
-    return a;
+    return a*d + b;
 }
 
-bool p2possible(long target, long[] l, long sum = 0, int i = 0) {
+bool possible(bool canCat = false)(long target, long[] l, long sum = 0, int i = 0) {
+    if (sum > target) return false;
     if (i == l.length) return sum == target;
-    state cur = state(sum, i);
-    if (cur in dp) return dp[cur];
-    return dp[cur] = p2possible(target, l, sum + l[i], i + 1)
-                  || p2possible(target, l, sum == 0 ? l[i] : sum * l[i], i + 1)
-                  || p2possible(target, l, cat(sum, l[i]), i+1);
+    return possible!canCat(target, l, sum + l[i], i + 1)
+        || possible!canCat(target, l, sum == 0 ? l[i] : sum * l[i], i + 1)
+        || (canCat && possible!canCat(target, l, cat(sum, l[i]), i+1));
 }
 
 void main() {
@@ -52,16 +32,13 @@ void main() {
         long target = to!long(ss[0]);
         long[] l = ss[1].split(" ").map!(to!long).array;
 
-        dp.clear();
         if (possible(target, l)) {
             part1 += target;
-        }
-        dp.clear();
-        if (p2possible(target, l)) {
+        } else if (possible!true(target, l)) {
             part2 += target;
         }
     }
 
     writeln("part 1: ", part1);
-    writeln("part 2: ", part2);
+    writeln("part 2: ", part1+part2);
 }
