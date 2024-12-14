9c723cb35fd6633003a3458a2ab7a87c7c8b4ad0
Matija Dizdar <matija.dizdar@protonmail.com>
2024-12-14 19:36:24 +0100

    day14 2024 decrapification

--- 2024/day14.d
+++ 2024/day14.d
@@ -4,7 +4,7 @@ import core.memory, core.thread.osthread, std.datetime;
 import std.typecons;
 
 auto readLines(string file)() {
-    return import(file).split("\n").filter!(l => l.length != 0);
+    return import(file).strip('\n').split("\n");
 }
 
 union Input {
@@ -22,6 +22,12 @@ Input parse(const ref string line) {
     return Input(matchAll(line, ctRegex!(`(-?\d+)`)).map!"a.hit.to!int".staticArray!4);
 }
 
+Input move(int w, int h, int seconds)(Input input) {
+    input.px = (input.px+seconds*(input.vx+w))%w;
+    input.py = (input.py+seconds*(input.vy+h))%h;
+    return input;
+}
+
 void main() {
     GC.disable;
 
@@ -33,37 +39,20 @@ void main() {
 
     int[4] q = 0;
 
-    foreach (input; readLines!"input/14.in".map!parse) {
-        input.px = (input.px+input.vx*100+100*w)%w;
-        input.py = (input.py+input.vy*100+100*h)%h;
+    foreach (input; readLines!"input/14.in".map!(l => l.parse.move!(w, h, 100))) {
         if (input.px == w/2 || input.py == h/2) continue;
-
         q[(input.px < w/2)*2 + (input.py < h/2)] += 1;
     }
 
     writeln("part 1: ", q.fold!"a*b"(1));
     
-    Input[] inputs = readLines!"input/14.in".map!parse.array;
-    foreach (ref input; inputs) {
-        input.px = (input.px+68*(input.vx+w))%w;
-        input.py = (input.py+68*(input.vy+h))%h;
-    }
+    Input[] inputs = readLines!"input/14.in".map!(l => l.parse.move!(w, h, 68)).array;
     // just draw until you find it lol
     for (int k = 68; true; k += 101) {
-        foreach (ref input; inputs) {
-            input.px = (input.px+101*(input.vx+w))%w;
-            input.py = (input.py+101*(input.vy+h))%h;
-        }
+        inputs.each!((ref a) => a.move!(w, h, 101));
         for (int i = 0; i < h; ++i) {
             for (int j = 0; j < w; ++j) {
-                foreach (const ref input; inputs) {
-                    if (input.px == j && input.py == i) {
-                        write("x");
-                        goto miran;
-                    }
-                }
-                write(".");
-miran:
+                write(inputs.any!(a => a.px == j && a.py == i) ? "x" : ".");
             }
             writeln();
         }
